046a1df0a5ff8bc5025ad7de7b6faa470699e7f2
Sanx78 <63214516+Sanx78@users.noreply.github.com>
2020-04-11 07:47:23 +1000

    Additional functions
    Some additional functions for managing meetings and roles
    Minor bugfixes

--- ZoomLibrary.psm1
+++ ZoomLibrary.psm1
@@ -1,4 +1,4 @@
-Enum ZoomLicenseType {
+﻿Enum ZoomLicenseType {
     Basic = 1
     Licensed = 2
     OnPrem = 3
@@ -85,7 +85,7 @@ function Get-ZoomGroup() {
 function Add-ZoomGroup() {
     Param([Parameter(Mandatory=$true)][System.String]$groupName)
     $group = Get-ZoomGroup -groupName $groupName
-    if ($group -ne $null) {
+    if ($null -ne $group) {
         Write-Error "Add-ZoomGroup: Group name ""$groupName"" already exists."
         return $null
     }
@@ -110,7 +110,7 @@ function Add-ZoomGroup() {
 function Remove-ZoomGroup() {
     Param([Parameter(Mandatory=$true)][System.String]$groupName)
     $group = Get-ZoomGroup -groupName $groupName
-    if ($group -eq $null) {
+    if ($null -eq $group) {
         Write-Error "Remove-ZoomGroup: Group name ""$groupName"" could not be found."
         return $null
     }
@@ -170,7 +170,7 @@ function Get-ZoomUsers() {
 function Get-ZoomGroupUsers() {
     Param([Parameter(Mandatory=$true)][System.String]$groupName)
     $group = Get-ZoomGroup -groupName $groupName
-    if ($group -eq $null) {
+    if ($null -eq $group) {
         Write-Error "Get-ZoomGroupUsers: Group name ""$groupName"" could not be found."
         return $null
     }
@@ -209,7 +209,7 @@ function Get-ZoomGroupUsers() {
 function Add-ZoomUsersToGroup() {
     Param([Parameter(Mandatory=$true)]$emails, [Parameter(Mandatory=$true)][System.String]$groupName)
     $group = Get-ZoomGroup -groupName $groupName
-    if ($group -eq $null) {
+    if ($null -eq $group) {
         Write-Error "Add-ZoomUsersToGroup: Group name ""$groupName"" could not be found."
         return $null
     }
@@ -297,7 +297,7 @@ function Set-ZoomUserDetails() {
     $params = @{}
     if ($firstName -ne [System.String]::Empty ) { $params.Add("first_name", $firstName) }
     if ($lastName -ne [System.String]::Empty) { $params.Add("last_name", $lastName) }
-    if ($license -ne $null) { $params.Add("type", $license) }
+    if ($null -ne $license) { $params.Add("type", $license) }
     if ($timezone -ne [System.String]::Empty) { $params.Add("timezone", $timezone) }
     if ($jobTitle -ne [System.String]::Empty) { $params.Add("job_title", $jobTitle) }
     if ($company -ne [System.String]::Empty) { $params.Add("company", $company) }
@@ -318,23 +318,21 @@ function Set-ZoomUserDetails() {
     The email address of the user
 
     .Parameter Password
-    A SecureString value of the password to set
+    The password to set
 
     .Example
-    Set-ZoomUserPassword -email "jerome@cactus.email" -password (ConvertTo-SecureString -String "Marketing101!" -Force -AsPlainText)
+    Set-ZoomUserPassword -email "jerome@cactus.email" -password "Marketing101!"
 #>
 function Set-ZoomUserPassword() {
     #' Performs no validation on password length and complexity
-    Param([Parameter(Mandatory=$true)][System.String]$email, [Parameter(Mandatory=$true)][SecureString]$password)
+    Param([Parameter(Mandatory=$true)][System.String]$email, [Parameter(Mandatory=$true)][System.String]$password)
     if ( (Get-ZoomUserExists -email $email) -eq $false ) {
         Write-Error "Set-ZoomUserPassword: User ""$email"" could not be found."
         return $null
     }
 
     Try {
-        $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
-        $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-        $result = Invoke-WebRequest -Uri "https://api.zoom.us/v2/users/$email/password" -Headers $headers -Body (@{"password" = $UnsecurePassword} | ConvertTo-Json) -Method PUT -ErrorAction Stop
+        Invoke-WebRequest -Uri "https://api.zoom.us/v2/users/$email/password" -Headers $headers -Body (@{"password" = $password} | ConvertTo-Json) -Method PUT -ErrorAction Stop
     } Catch {
         $response = $_
         $message = ($response.ErrorDetails.Message | ConvertFrom-Json).message
@@ -387,11 +385,40 @@ function Set-ZoomUserStatus() {
     .Parameter License
     The type of license to apply. Valid values are: [ZoomLicenseType]::Basic, [ZoomLicenseType]::Licensed and [ZoomLicenseType]::OnPrem
 
+    .Parameter TimeZone
+    The time zone the Zoom user is in. Time zone to be specific in TZD format. For more information, see: https://marketplace.zoom.us/docs/api-reference/other-references/abbreviation-lists#timezones
+
+    .Parameter JobTitle
+    The Zoom user's job title
+
+    .Parameter Company
+    The Zoom user's company
+
+    .Parameter Location
+    The Zoom user's location. This is a non-validated field and may be used to represent any location information: office name, city, country, physical address, etc.
+
+    .Parameter PhoneNumber
+    The Zoom user's phone number, to be specified with country code, e.g. +44 207 782 5000
+
+    .Parameter GroupName
+    The name of the group
+
     .Example
-    Add-ZoomUser -email "francisco@cactus.email" -firstName Francisco -lastName Hunter -license Basic
+    Add-ZoomUser -email "francisco@cactus.email" -firstName Francisco -lastName Hunter -license Basic timezone "Europe/London" -jobTitle "Gra[hic Artist" -company "Cactus Industries (Europe)" -Location Basildon -phoneNumber "+44 1268 533333" -groupName "Designers"
 #>
 function Add-ZoomUser() {
-    Param([Parameter(Mandatory=$true)][System.String]$email, [Parameter(Mandatory=$true)][System.String]$firstName, [Parameter(Mandatory=$true)][System.String]$lastName, [Parameter(Mandatory=$true)][ZoomLicenseType]$license)
+    Param(
+        [Parameter(Mandatory=$true)][System.String]$email,
+        [Parameter(Mandatory=$true)][System.String]$firstName,
+        [Parameter(Mandatory=$true)][System.String]$lastName,
+        [Parameter(Mandatory=$true)][ZoomLicenseType]$license,
+        [System.String]$timezone,
+        [System.String]$jobTitle,
+        [System.String]$company,
+        [System.String]$location,
+        [System.String]$phoneNumber,
+        [System.String]$groupName
+        )
     if ( (Get-ZoomUserExists -email $email) -eq $true ) {
         Write-Error "Add-ZoomUser: User ""$email"" already exists."
         return $null
@@ -405,6 +432,8 @@ function Add-ZoomUser() {
     $body.Add("action", "create")
     $body.Add("user_info", $userinfo)
     Invoke-RestMethod -Uri "https://api.zoom.us/v2/users" -Headers $headers -Body ( $body | ConvertTo-Json) -Method POST
+    Set-ZoomUserDetails -email $email -timezone $timezone -jobTitle $jobTitle -company $company -location $location -phoneNumber $phoneNumber
+    Add-ZoomUsersToGroup -emails $email -groupName $groupName
 }
 
 <#
@@ -482,7 +511,7 @@ function Add-ZoomUserAssistant() {
     }
     $body = New-Object -TypeName psobject
     $body | Add-Member -Name assistants -Value @(@{"email" = $assistant}) -MemberType NoteProperty
-    $result = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/assistants" -Headers $headers -Body ($body | ConvertTo-Json) -Method POST
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/assistants" -Headers $headers -Body ($body | ConvertTo-Json) -Method POST
 }
 
 <#
@@ -511,8 +540,8 @@ function Remove-ZoomUserAssistant() {
         Write-Error "Remove-ZoomUserAssistants: Assistant ""$assistant"" could not be found."
         return $null
     }
-    $result = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/assistants/$assistant" -Headers $headers -Method DELETE
-    $result = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/schedulers/$assistant" -Headers $headers -Method DELETE
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/assistants/$assistant" -Headers $headers -Method DELETE
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/schedulers/$assistant" -Headers $headers -Method DELETE
 }
 
 <#
@@ -557,14 +586,14 @@ function Get-ZoomMeetings() {
         Write-Error "Get-ZoomMeetings: User ""$email"" could not be found."
         return $null
     }
-    if ($meetingType -eq $null) { $meetingType = [ZoomMeetingType]::Live }
+    if ($null -eq $meetingType) { $meetingType = [ZoomMeetingType]::Live }
     $zoommeetings = @()
     $page1 = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/meetings?page_size=100&type=$($meetingType.ToString().ToLower())" -Headers $headers
     Write-Debug "Get-ZoomMeetings: $($page1.total_records) meetings in $($page1.page_count) pages. Adding page 1 containing $($page1.meetings.count) records."
     $zoommeetings += $page1.meetings
     if ($page1.page_count -gt 1) {
         for ($count=2; $count -le $page1.page_count; $count++) {
-            $page = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/meetings?page_size=100&type=$($test.ToString().ToLower())&page_number=$page" -Headers $headers
+            $page = Invoke-RestMethod -Uri "https://api.zoom.us/v2/users/$email/meetings?page_size=100&type=$($meetingType.ToString().ToLower())&page_number=$page" -Headers $headers
             Write-Debug "Get-ZoomMeetings: Adding page $count containing $($page.meetings.count) records."
             $zoommeetings += $page.meetings
             Start-Sleep -Milliseconds 100 #` To keep under Zoom's API rate limit of 10 per second.
@@ -608,9 +637,245 @@ function Get-ZoomMeetingDetails() {
 #>
 function Stop-ZoomMeeting() {
     Param([Parameter(Mandatory=$true)][System.Int32]$meetingID)
-    $meeting = Invoke-RestMethod -Uri "https://api.zoom.us/v2/meetings/$meetingID/status" -Headers $headers -Body ( @{"action" = "end"} | ConvertTo-Json) -Method PUT
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/meetings/$meetingID/status" -Headers $headers -Body ( @{"action" = "end"} | ConvertTo-Json) -Method PUT
 }
 
+<#
+    .Synopsis
+    Lists Zoom roles
+
+    .Description
+    Returns an array of all Zoom roles
+
+    .Example
+    Get-ZoomRoles
+#>
+function Get-ZoomRoles() {
+    $roles = Invoke-RestMethod -Uri "https://api.zoom.us/v2/roles" -Headers $headers -Method Get
+    Write-Debug "Get-ZoomRole: Retrieved $($roles.total_records) groups."
+    return $roles.roles
+}
+
+<#
+    .Synopsis
+    Gets a Zoom role
+
+    .Description
+    Returns a specific Zoom role based on role name
+
+    .Parameter RoleName
+    The role name
+
+    .Example
+    Get-ZoomRole -roleName "Level 2 Service Desk"
+#>
+function Get-ZoomRole() {
+    Param([Parameter(Mandatory=$true)][System.String]$roleName)
+    $roles = Get-ZoomRoles
+    $thisRole = $roles | Where-object { $_.name -eq $roleName }
+    return $thisRole
+}
+
+<#
+    .Synopsis
+    Gets Zoom users in a role
+
+    .Description
+    Returns an array of Zoom users in a role, based upon role name
+
+    .Parameter RoleName
+    The role name
+
+    .Example
+    Get-ZoomRoleUsers -roleName "Level 2 Service Desk"
+#>
+function Get-ZoomRoleUsers() {
+    Param([Parameter(Mandatory=$true)][System.String]$roleName)
+    $role = Get-ZoomRole -roleName $roleName
+    if ($null -eq $role) {
+        Write-Error "Get-ZoomRoleUsers: Role ""$role"" could not be found."
+        return $null
+    }
+    $roleusers = @()
+    $page1 = Invoke-RestMethod -Uri "https://api.zoom.us/v2/roles/$($role.id)/members?page_size=100" -Headers $headers -Method Get
+    Write-Debug "Get-ZoomRoleUsers: $($page1.total_records) users in $($page1.page_count) pages. Adding page 1 containing $($page1.members.count) records."
+    $roleusers += $page1.members
+    if ($page1.page_count -gt 1) {
+        for ($count=2; $count -le $page1.page_count; $count++) {
+            $page = Invoke-RestMethod -Uri "https://api.zoom.us/v2/roles/$($role.id)/members?page_size=100&page_number=$count" -Headers $headers
+            Write-Debug "Get-ZoomRoleUsers: Adding page $count containing $($page.members.count) records."
+            $roleusers += $page.members
+            Start-Sleep -Milliseconds 100 #` To keep under Zoom's API rate limit of 10 per second.
+        }
+    }
+    return $roleusers
+}
+
+<#
+    .Synopsis
+    Sets a Zoom user's role
+
+    .Description
+    Sets a Zoom user's role based upon email address and role name
+
+    .Parameter Email
+    The email addresses of the users to add as an array of strings. Will also accept a string containing a single address.
+    Currently, no more than 30 users can be added to a role in a single call.
+
+    .Parameter RoleName
+    The role name
+
+    .Example
+    Set-ZoomRoleUsers -email "melissa@cactus.email" -roleName "Level 2 Service Desk"
+#>
+function Set-ZoomRoleUsers() {
+    Param([Parameter(Mandatory=$true)]$emails, [Parameter(Mandatory=$true)][System.String]$roleName)
+    if ($rolename.ToLower() -eq "owner" -or $roleName.ToLower() -eq "member") {
+        Write-Error "Set-ZoomRoleUsers: Users cannot be added to the Member or Owner roles."
+        return $null
+    }
+    $role = Get-ZoomRole -roleName $roleName
+    if ($null -eq $role) {
+        Write-Error "Set-ZoomRoleUsers: Role ""$role"" could not be found."
+        return $null
+    }
+    $members = @()
+    $emails | ForEach-Object {
+        if ( (Get-ZoomUserExists -email $_) -eq $false ) {
+            Write-Error "Set-ZoomRoleUsers: User ""$_"" could not be found."
+            return $null
+        }
+        $member = New-Object -TypeName psobject
+        $member | Add-Member -Name email -Value $_ -MemberType NoteProperty
+        $members += $member
+    }
+    $body = New-Object -TypeName psobject
+    $body | Add-Member -Name members -Value $members -MemberType NoteProperty
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/roles/$($role.id)/members" -Headers $headers -Body ($body | ConvertTo-Json) -Method Post
+}
+
+<#
+    .Synopsis
+    Removes a Zoom user from a role
+
+    .Description
+    Removes a Zoom user from a role based upon email address and role name
+
+    .Parameter Email
+    The email address of the user to remove from the role
+
+    .Parameter RoleName
+    The role name
+
+    .Example
+    Remove-ZoomRoleUser -email "melissa@cactus.email" -roleName "Level 2 Service Desk"
+#>
+function Remove-ZoomRoleUser() {
+    Param([Parameter(Mandatory=$true)][System.String]$email, [Parameter(Mandatory=$true)][System.String]$roleName)
+    $role = Get-ZoomRole -roleName $roleName
+    if ($null -eq $role) {
+        Write-Error "Remove-ZoomRoleUser: Role ""$role"" could not be found."
+        return $null
+    }
+    if ( (Get-ZoomUserExists -email $email) -eq $false ) {
+        Write-Error "Remove-ZoomRoleUser: User ""$email"" could not be found."
+        return $null
+    }
+    Invoke-RestMethod -Uri "https://api.zoom.us/v2/roles/$($role.id)/members/$email" -Headers $headers -Method Delete
+}
+
+<#
+    .Synopsis
+    Generates the Zoom daily usage report
+
+    .Description
+    Generates the Zoom daily usage report for any given month. Reports the daily number of new users, meerings, participants and meeting minutes.
+
+    .Parameter Year
+    The year of the date to report on, in yyyy format.
+
+    .Parameter Month
+    The month of the date to report on.
+
+    .Example
+    Get-ZoomUsageReport -year 2020 -month 01
+#>
+function Get-ZoomUsageReport() {
+    Param([System.Int32]$month, [System.Int32]$year)
+    if ($null -ne $month -and $null -ne $year) {
+        $uri = "https://api.zoom.us/v2/report/daily?year=$year&month=$month"
+    } else {
+        $uri = "https://api.zoom.us/v2/report/daily"
+    }
+    $report = Invoke-RestMethod -Uri $uri -Headers $headers
+    return $report
+}
+
+<#
+    .Synopsis
+    Generates the Zoom user meetings report
+
+    .Description
+    Generates the Zoom meetings report for a specific user in a defined timespan. Note: the timespan defined by the -from and -to parameters cannot be more than one month in duration.
+
+    .Parameter Email
+    The email address of the user
+
+    .Parameter From
+    The start date of the report, expressed as a datetime object.
+    .Parameter From
+    The end date of the report, expressed as a datetime object.
+
+    .Example
+    Get-ZoomMeetingReport -email "jerome@cactus.email" -from "2020-01-01" -to "2020-02-01"
+#>
+function Get-ZoomMeetingReport() {
+    Param([Parameter(Mandatory=$true)][System.String]$email,
+    [Parameter(Mandatory=$true)][System.DateTime]$from,
+    [Parameter(Mandatory=$true)][System.DateTime]$to)
+
+    if ( (Get-ZoomUserExists -email $email) -eq $false ) {
+        Write-Error "Get-ZoomMeetingReport: User ""$email"" could not be found."
+        return $null
+    }
+    [System.Timespan]$timeDifference = $to - $from
+    if ($timeDifference.TotalDays -gt 31) {
+        Write-Error "Get-ZoomMeetingReport: The date range defined by the -from and -to parameters cannot be more than one month apart."
+        return $null
+    }
+    $zoommeetings = @()
+    $page1 = Invoke-RestMethod -Uri "https://api.zoom.us/v2/report/users/$email/meetings?from=$($from.ToString("yyyy-MM-dd"))&to=$($to.ToString("yyyy-MM-dd"))&page_size=100" -Headers $headers
+    Write-Debug "Get-ZoomMeetingReport: $($page1.total_records) meetings in $($page1.page_count) pages. Adding page 1 containing $($page1.meetings.count) records."
+    $zoommeetings += $page1.meetings
+    if ($page1.page_count -gt 1) {
+        for ($count=2; $count -le $page1.page_count; $count++) {
+            $page = Invoke-RestMethod -Uri "https://api.zoom.us/v2/report/users/$email/meetings?from=$($from.ToString("yyyy-MM-dd"))&to=$($to.ToString("yyyy-MM-dd"))&page_size=100&page_number=$page" -Headers $headers
+            Write-Debug "Get-ZoomMeetingReport: Adding page $count containing $($page.meetings.count) records."
+            $zoommeetings += $page.meetings
+            Start-Sleep -Milliseconds 100 #` To keep under Zoom's API rate limit of 10 per second.
+        }
+    }
+    return $zoommeetings
+}
+
+<#
+    .Synopsis
+    Generates the Zoom meeting participant report
+
+    .Description
+    Generates the Zoom participant report for a specific meeting
+
+    .Parameter MeetingID
+    The ID of the meeting. Can be the meeting ID or the meeting instance UUID. If the meeting ID is passed, it will show details of the last instance of that meeting.
+
+    .Example
+    Get-ZoomUsageReport -year 2020 -month 01
+#>
+function Get-ZoomMeetingParticipantReport() {
+    Param([Parameter(Mandatory=$true)][System.String]$meetingID)
+    $report = Invoke-RestMethod -Uri "https://api.zoom.us/v2/report/meetings/$meetingID/participants?page-size=300" -Headers $headers
+    return $report
+}
 #`------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -642,3 +907,11 @@ Export-ModuleMember -Function Remove-ZoomUserAssistant -Alias rzua
 Export-ModuleMember -Function Get-ZoomMeetings -Alias gzm
 Export-ModuleMember -Function Get-ZoomMeetingDetails -Alias gzmd
 Export-ModuleMember -Function Stop-ZoomMeeting -Alias szm
+Export-ModuleMember -Function Get-ZoomRoles -Alias gzrs
+Export-ModuleMember -Function Get-ZoomRole -Alias gzr
+Export-ModuleMember -Function Get-ZoomRoleUsers -Alias gzru
+Export-ModuleMember -Function Set-ZoomRoleUsers -Alias szru
+Export-ModuleMember -Function Remove-ZoomRoleUser -Alias rzru
+Export-ModuleMember -Function Get-ZoomUsageReport -Alias gzur
+Export-ModuleMember -Function Get-ZoomMeetingReport -Alias gzmr
+Export-ModuleMember -Function Get-ZoomMeetingParticipantReport -Alias gzmpr
